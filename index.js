f3cae62e7a5afe54b8a751e01b70778b60035bb0
shanin18 <112709243+shanin18@users.noreply.github.com>
2023-06-09 01:18:16 +0600

    added get operation on popularInstructor & popularClasses

--- index.js
+++ index.js
@@ -8,6 +8,68 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gacal02.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const classesCollection = client
+      .db("capturedVisions")
+      .collection("classes");
+    const InstructorsCollection = client
+      .db("capturedVisions")
+      .collection("instructors");
+
+    app.get("/popularClasses", async (req, res) => {
+      const query = {
+        enrolled: { $gt: 2000 },
+        availableSeats: { $gt: 0 },
+      };
+
+      const options = {
+        projection: { name: 1, image: 1, instructor: 1, enrolled: 1 },
+      };
+
+      const result = await classesCollection.find(query, options).toArray();
+      res.send(result);
+    });
+
+    app.get("/popularInstructors", async (req, res) => {
+      const query = {
+        students: { $gt: 5000 },
+      };
+
+      const options = {
+        projection: { name: 1, image: 1, students: 1 },
+      };
+
+      const result = await InstructorsCollection.find(query, options).toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("captured vision is running....");
 });
